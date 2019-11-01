0a4f981be238d445f6ca58d4436f895ec524ab92
juliafealves <giT#8985>
2019-11-01 15:12:05 -0300

    Criado o middleware para verificar token de acesso do usuário.

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization)
+    return res.status(401).json({ error: 'Token not provided.' });
+
+  const [, token] = authorization.split(' ');
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token is invalid.' });
+  }
+};
