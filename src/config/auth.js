d659895445a9c00272b0271ba94b4b95384cd52e
juliafealves <giT#8985>
2019-11-01 14:33:26 -0300

    Adicionada a funcionalidade de autenticação.

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  expiresIn: '7d',
+  secret: 'b0ea55b226db3506fc2cbae156bc1fd4',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 routes.get('/', (req, res) => res.json({ ok: true }));
+routes.post('/sessions', SessionController.store);
 
 export default routes;
